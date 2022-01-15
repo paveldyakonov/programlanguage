f8caab64ef9cf7a8f66883c784ccffd50c196cfd
Pavel <pashadyakonov27082003@gmail.com>
2022-01-15 15:20:04 +0900

    Laba6.5

--- Laba6.4/Laba6.4.cpp
+++ Laba6.4/Laba6.4.cpp
@@ -4,30 +4,52 @@
 #include <iostream>
 #include <list>
 #include <string>
-
+#include <map>
 using std::cout;
 using std::cin;
 using std::endl;
 using std::list;
 using std::string;
+using std::pair;
+using std::map;
 void Mylist()
 {
-    string s;
+    
+    string s,s1;
     list <string> l1;
+    map <string,string> mp;
     cout << "Введите текст: \n";
-    while (s != ".")
+    while ((s != ".")and(s1 != "."))
     {
         getline(cin, s);
+        getline(cin, s1);
         if (s == ".") break;
-        l1.push_back(s);
+        if (s1 == ".") break;
+        //l1.push_back(s);
+        pair <string, string> p1 = make_pair(s, s1);
+        
+        mp[s] +="\n"+s1;
+        l1.push_back(s1);
+        
+        //mp.insert(p1);
+        //l1.push_back(s1);
     }
-    
+    map <string, string> ::iterator it = mp.begin();
+    cout << "Список: " << endl;
+    for (int i = 1; it != mp.end(); it++, i++) {  // выводим их
+        cout <<"\n"<< i << ") Группа: " << it->first << " \nУчастники: " << it->second << endl;
+    }
+    /*while (mp.empty() == false)
+    {
+        cout << mp.begin();
+        mp.erase(mp.begin());
+    }
     l1.sort();
     while (l1.empty() == false)
     {
         cout << "\n" << l1.front();
         l1.pop_front();
-    }
+    }*/
 }
 int main()
 {
